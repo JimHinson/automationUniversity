78f8bb68a644db641b567d483dffcae7fa8605a9
Jimmy Hinson <jimmy.hinson@cengage.com>
2020-06-02 07:53:21 -0400

    Branch1 goals: Understand unit test framework and how this fits in with Java.

--- src/test/java/com/cengage/webassign/BaseUtils/WebDriverFactory.java
+++ src/test/java/com/cengage/webassign/BaseUtils/WebDriverFactory.java
@@ -1,18 +1,29 @@
 package com.cengage.webassign.BaseUtils;
 import com.cengage.webassign.framework.core.WebAssignException;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import com.cengage.webassign.framework.core.ChromeWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class WebDriverFactory  {
 	protected WebDriverFactory(){
 }
 
 	public static WebDriver composeWebDriver(String browser, String properties, int implicitWait) throws WebAssignException {
-		WebDriver webDriver = null;
-		if (browser.equalsIgnoreCase("chrome")) {
-			ChromeWebDriver chromeWebDriver = new ChromeWebDriver();
-			webDriver = chromeWebDriver.compose(properties, implicitWait);
-		}
+		WebDriver webDriver;
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments(properties);
+		WebDriverManager.chromedriver().setup();
+		webDriver = new ChromeDriver(options);
+		webDriver.manage().window().maximize();
+		webDriver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS);
+//		return driver;
+//		WebDriver webDriver = null;
+//			ChromeWebDriver chromeWebDriver = new ChromeWebDriver();
+//			webDriver = chromeWebDriver.compose(properties, implicitWait);
 		return webDriver;
 	}
 }
--- src/test/java/com/cengage/webassign/Tests/WA_SmokeTest.java
+++ src/test/java/com/cengage/webassign/Tests/WA_SmokeTest.java
@@ -1,43 +1,32 @@
 package com.cengage.webassign.Tests;
 
 import com.cengage.webassign.BaseUtils.WebDriverFactory;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import com.cengage.webassign.Utils.PropfileReader;
 import com.cengage.webassign.framework.core.BaseTest;
-import com.cengage.webassign.pageObjects.HomePage;
-import com.cengage.webassign.pageObjects.LoginPage;
 
 public class WA_SmokeTest extends BaseTest {
-	
-//	HomePage homePage;
-	/**
-	 * TODO: move timeout to DefaultSettings.properties
-	 * capture screenshot on failure
-	 */
-	
+
 	@BeforeTest //This will run before the tests to ensure test setup
 	public void testSetup() throws Exception {
 
 	}
 
-	@Test
-	public void loadHomepage() throws Exception {
-		this.driver = WebDriverFactory.composeWebDriver("chrome", "--disable-logging", 10000);
-	}
-	
 	@BeforeMethod // This will run before each test to remove dependency of any test from another
 	public void launchHomePage() {
 	}
-	
 
-	
+	@Test
+	public void loadHomepage() throws Exception {
+		this.driver = WebDriverFactory.composeWebDriver("chrome", "--disable-logging", 10000);
+		driver.get("https://www.wa-staging.net/wa-auth/login");
+		java.lang.Thread.sleep(2000);
+	}
+
 	@AfterTest //This will run after all the tests to close current browser instance
 	public void closeBrowser() {
 		driver.close();
-//		homePage.close();
 	}
 }
